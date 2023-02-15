e240169d947f68c6769babad54d4ab12c809d8f6
Bettina Staartjes <bbstaars@gmail.com>
2023-02-14 19:22:17 -0500

    reinstalling dependencies

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,54 +1,68 @@
-//fix this one!
-
-const connection = require('../config/connection');
-const { Post, Tags } = require('../models');
-// Import functions for seed data
-const { getRandomColor, getRandomPost, genRandomIndex } = require('./data');
-
-// Start the seeding runtime timer
-console.time('seeding');
-
-// Creates a connection to mongodb
-connection.once('open', async () => {
-  // Delete the entries in the collection
-  await Post.deleteMany({});
-  await Tags.deleteMany({});
-
-  // Empty arrays for randomly generated posts and tags
-  const tags = [];
-  const posts = [];
-
-  // Function to make a post object and push it into the posts array
-  const makePost = (text) => {
-    posts.push({
-      published: Math.random() < 0.5,
-      text,
-      tags: [tags[genRandomIndex(tags)]._id],
-    });
+const addDateSuffix = (date) => {
+    let dateStr = date.toString();
+  
+    // get last char of date string
+    const lastChar = dateStr.charAt(dateStr.length - 1);
+  
+    if (lastChar === '1' && dateStr !== '11') {
+      dateStr = `${dateStr}st`;
+    } else if (lastChar === '2' && dateStr !== '12') {
+      dateStr = `${dateStr}nd`;
+    } else if (lastChar === '3' && dateStr !== '13') {
+      dateStr = `${dateStr}rd`;
+    } else {
+      dateStr = `${dateStr}th`;
+    }
+  
+    return dateStr;
   };
-
-  // Create 20 random tags and push them into the tags array
-  for (let i = 0; i < 20; i++) {
-    const tagname = getRandomColor();
-
-    tags.push({
-      tagname,
-      color: tagname,
-    });
-  }
-
-  // Wait for the tags to be inserted into the database
-  await Tags.collection.insertMany(tags);
-
-  // For each of the tags that exist, make a random post of length 50
-  tags.forEach(() => makePost(getRandomPost(50)));
-
-  // Wait for the posts array to be inserted into the database
-  await Post.collection.insertMany(posts);
-
-  // Log out a pretty table for tags and posts, excluding the excessively long text property
-  console.table(tags);
-  console.table(posts, ['published', 'tags', '_id']);
-  console.timeEnd('seeding');
-  process.exit(0);
-});
+  
+  // function to format a timestamp, accepts the timestamp and an `options` object as parameters
+  module.exports = (
+    timestamp,
+    { monthLength = 'short', dateSuffix = true } = {}
+  ) => {
+    // create month object
+    const months = {
+      0: monthLength === 'short' ? 'Jan' : 'January',
+      1: monthLength === 'short' ? 'Feb' : 'February',
+      2: monthLength === 'short' ? 'Mar' : 'March',
+      3: monthLength === 'short' ? 'Apr' : 'April',
+      4: monthLength === 'short' ? 'May' : 'May',
+      5: monthLength === 'short' ? 'Jun' : 'June',
+      6: monthLength === 'short' ? 'Jul' : 'July',
+      7: monthLength === 'short' ? 'Aug' : 'August',
+      8: monthLength === 'short' ? 'Sep' : 'September',
+      9: monthLength === 'short' ? 'Oct' : 'October',
+      10: monthLength === 'short' ? 'Nov' : 'November',
+      11: monthLength === 'short' ? 'Dec' : 'December',
+    };
+  
+    const dateObj = new Date(timestamp);
+    const formattedMonth = months[dateObj.getMonth()];
+  
+    const dayOfMonth = dateSuffix
+      ? addDateSuffix(dateObj.getDate())
+      : dateObj.getDate();
+  
+    const year = dateObj.getFullYear();
+    let hour =
+      dateObj.getHours() > 12
+        ? Math.floor(dateObj.getHours() - 12)
+        : dateObj.getHours();
+  
+    // if hour is 0 (12:00am), change it to 12
+    if (hour === 0) {
+      hour = 12;
+    }
+  
+    const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes();
+  
+    // set `am` or `pm`
+    const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
+  
+    const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
+  
+    return formattedTimeStamp;
+  };
+  
