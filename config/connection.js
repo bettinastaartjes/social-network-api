2511bc5d40a9db109f934cdce1fd3d08a75e84f8
Bettina Staartjes <bbstaars@gmail.com>
2023-02-15 12:51:15 -0500

    updating mongoose connect in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI ||
-    'mongodbL//127.0.0.1:27017/socialmedia', {
+    'mongodb://127.0.0.1:27017/thoughtData', {
         userNewUrlParser: true,
         useUnifiedTopology: true,
     });
